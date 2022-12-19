2b6944c254466d3c4fa65535a1827e6cc3cc45df
Khaled Ebrahem <72916259+khaledbibo815@users.noreply.github.com>
2022-12-20 01:25:59 +0200

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,21 +1,15 @@
 def insertion_sort():
-  # get a list of numbers from the user
+  
   numbers = input("Enter a list of numbers, separated by spaces: ").split()
   numbers = [int(x) for x in numbers]
 
-  # start at the second element of the list (the first one is already sorted)
+  
   for i in range(1, len(numbers)):
-    # store the current value to be sorted
     current_value = numbers[i]
-    # start the "hole" at the current position
     hole = i
-    # keep moving the hole to the left as long as the value at the hole is greater than the current value
     while hole > 0 and numbers[hole - 1] > current_value:
-      # move the value at the hole to the right
       numbers[hole] = numbers[hole - 1]
-      # move the hole to the left
       hole -= 1
-    # put the current value into the hole
     numbers[hole] = current_value
   return numbers
 
